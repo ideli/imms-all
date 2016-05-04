6fb4eba10287c59e7d003a9261db66ca5feb557e
wangping <wangping19880131@163.com>
2016-05-05 00:20:35 +0800

    加入错误的单测

---
+++ imms-system/src/test/java/com.hisign/MyTest.java
@@ -0,0 +1,18 @@
+package com.hisign;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 测试
+ * @author wangping
+ * @version 1.0
+ * @since 2016/5/5 0:18
+ */
+public class MyTest {
+
+    @Test
+    public void thisTest() {
+        Assert.assertTrue(1 == 2);
+    }
+}
