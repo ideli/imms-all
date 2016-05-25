b95afc3d446fd574444cadced5e3cdf837ad1bfd
xhwPC <565972196@qq.com>
2016-05-25 17:19:40 +0800

    'less'

--- imms-web/src/main/webapp/gulpfile.js
+++ imms-web/src/main/webapp/gulpfile.js
@@ -3,17 +3,43 @@ var rename = require('gulp-rename');
 
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
 var del = require('del');
 
 
 gulp.task('hint', function() {
-    gulp.src('js/*.js')
+    gulp.src('script/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['hint']);
+    gulp.watch('script/*.js', ['hint']);
+});
+
+
+gulp.task('less', function () {
+    gulp.src('style/base.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
+    gulp.src('style/index.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minifycss', function() {
+    return gulp.src('dist/css/base.css')
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('browserify', function(){
+    return browserify('script/base.js')
+        //.transform(babelify)
+       // .bundle()
+       // .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('uglify', function() {
