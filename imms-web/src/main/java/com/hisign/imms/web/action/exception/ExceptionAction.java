9d9831a20f0b028820b2bcfc199ca1502ad301ad
wangping <wangping@hisign.com.cn>
2016-05-27 07:29:26 +0800

    修改exceptionAction目录

--- imms-web/src/main/java/com/hisign/imms/web/action/exception/ExceptionAction.java
+++ imms-web/src/main/java/com/hisign/imms/web/action/exception/ExceptionAction.java
@@ -1,4 +1,4 @@
-package com.hisign.imms.web.action;
+package com.hisign.imms.web.action.exception;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
