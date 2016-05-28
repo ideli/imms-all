6c8b8ceceb4b6fdd5c246bb0a7198fa87cf13830
wangping <wangping@hisign.com.cn>
2016-05-28 10:34:44 +0800

    加入json字符串转为对象的注解

---
+++ imms-web/src/main/java/com/hisign/imms/web/bind/annotation/TranslateObject.java
@@ -0,0 +1,29 @@
+package com.hisign.imms.web.bind.annotation;
+
+import com.hisign.imms.Constants;
+
+import java.lang.annotation.*;
+
+/**
+ * 将json字符串转换为对象
+ * @author wangping
+ * @version 1.0
+ * @since 2016/5/28 10:17
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface TranslateObject {
+
+    /**
+     * json字符串对应的对象类型名
+     * @return
+     */
+    String className() default "java.util.HashMap";
+
+    /**
+     * 传入的json字符串
+     * @return
+     */
+    String value() default "{}";
+}
---
+++ imms-web/src/main/java/com/hisign/imms/web/bind/method/TranslateObjectMethodArgumentsResolver.java
@@ -0,0 +1,30 @@
+package com.hisign.imms.web.bind.method;
+
+import com.alibaba.fastjson.JSON;
+import com.hisign.imms.web.bind.annotation.CurrentUser;
+import com.hisign.imms.web.bind.annotation.TranslateObject;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * 用于绑定@TranslateObject的方法参数解析器
+ * @author wangping
+ * @version 1.0
+ * @since 2016/5/28 10:19
+ */
+public class TranslateObjectMethodArgumentsResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(TranslateObject.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        TranslateObject translateObjectAnnotation = parameter.getParameterAnnotation(TranslateObject.class);
+        Object obj = JSON.parseObject(translateObjectAnnotation.value(), Class.forName(translateObjectAnnotation.className()));
+        return obj;
+    }
+}
